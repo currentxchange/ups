f542b1f4bc0198b0245e82e192261dcef0e2d4e2
Douglas Butner <dougbutner@gmail.com>
2024-02-04 11:04:15 -0600

    ADDED integers m49 values

--- contracts/include/continents.hpp
+++ contracts/include/continents.hpp
@@ -1,9 +1,5 @@
 /*/ 
-
-This header contains efficient enumerations of continents to use in C++ contracts for WAX blockchains.
-
 Enums are not explicitly numbered because the UN does not produce standardizes numbers for ISO 3166 nor M49 (Antarctica has no code)
-
 /*/
 
 
@@ -28,29 +24,28 @@ enum class Regions_M49 {
 };
 
 enum class Continent_Subregions {
-    WORLD,                  // World
-    SOUTHERN_ASIA,          // Southern Asia
-    EASTERN_ASIA,           // Eastern Asia
-    SOUTH_EASTERN_ASIA,     // South-eastern Asia
-    EASTERN_AFRICA,         // Eastern Africa
-    SOUTH_AMERICA,          // South America
-    WESTERN_AFRICA,         // Western Africa
-    NORTHERN_AMERICA,       // Northern America
-    EASTERN_EUROPE,         // Eastern Europe
-    WESTERN_ASIA,           // Western Asia
-    NORTHERN_AFRICA,        // Northern Africa
-    WESTERN_EUROPE,         // Western Europe
-    MIDDLE_AFRICA,          // Middle Africa
-    CENTRAL_AMERICA,        // Central America
-    SOUTHERN_EUROPE,        // Southern Europe
-    NORTHERN_EUROPE,        // Northern Europe
-    CENTRAL_ASIA,           // Central Asia
-    SOUTHERN_AFRICA,        // Southern Africa
-    CARIBBEAN,              // Caribbean
-    AUSTRALIA_AND_NEW_ZEALAND, // Australia and New Zealand
-    MELANESIA,              // Melanesia
-    POLYNESIA,              // Polynesia
-    MICRONESIA,             // Micronesia
-    ANTARCTICA              // Antarctica
+    WORLD = 1,
+    SOUTHERN_ASIA = 34,
+    EASTERN_ASIA = 30,
+    SOUTH_EASTERN_ASIA = 35,
+    EASTERN_AFRICA = 14,
+    SOUTH_AMERICA = 5,
+    WESTERN_AFRICA = 11,
+    NORTHERN_AMERICA = 21,
+    EASTERN_EUROPE = 151,
+    WESTERN_ASIA = 145,
+    NORTHERN_AFRICA = 15,
+    WESTERN_EUROPE = 155,
+    MIDDLE_AFRICA = 17,
+    CENTRAL_AMERICA = 13,
+    SOUTHERN_EUROPE = 39,
+    NORTHERN_EUROPE = 154,
+    CENTRAL_ASIA = 143,
+    SOUTHERN_AFRICA = 18,
+    CARIBBEAN = 29,
+    AUSTRALIA_AND_NEW_ZEALAND = 53,
+    MELANESIA = 54,
+    POLYNESIA = 61,
+    MICRONESIA = 57,
+    ANTARCTICA = 10
 };
-
