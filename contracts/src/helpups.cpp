23385c13b8fb83ad582a4e01704f9272048ff19f
Douglas Butner <dougbutner@gmail.com>
2024-02-10 15:31:05 -0600

    ADDED upsert_total() helper function

--- contracts/src/helpups.cpp
+++ contracts/src/helpups.cpp
@@ -1,16 +1,123 @@
-void ups::updateup(uint32_t upscount, name upsender, uint64_t content_id) {
+/*/
+
+
+
+
+
+
+
+/*/
+
+
+
+// --- DISPATCHER Checks + calls logup() updateiou() and updatetotal() --- //
+void ups::upsertup(uint32_t upscount, name upsender, uint64_t content_id, bool negative) {
+      if (!negative){
+    // --- Log the ups in ups table --- // 
+    upsert_logup(upscount, upsender, content_id, negative);
     
+    // --- Calls action to update the TOTALS table -- //
+    upsert_total(upscount, upsender, content_id, negative);
+  }
+}//END upsertup()
+
+
+
+
+// --- Update running log of ups --- //
+void upsert_logup(uint32_t upscount, name upsender, uint32_t content_id, bool negative){
+  //NOTE negative should only be called for deletions (user gets removed from system)
+
+  // --- Add record to _ups --- //
+  _ups(get_self(), content_id); //WARN CHECK - is content_id right here? URGENT I really think this is wrong for all upserts
+  auto ups_iterator = _ups.find(upid);
+  uint32_t time_of_up = eosio::time_point_sec::sec_since_epoch();
+  if( ups_iterator == _ups.end() )
+  { // -- Make New Record
+    _ups.emplace(upsender, [&]( auto& row ) {//URGENT This needs to be changed when we figure out the PK issue
+      row.key = content_id;
+      row.totalups = newups;
+      row.tuid = momentu;
+    });
+  } 
+  else 
+  { // -- Update Record
+    if (negative){
+      _ups.modify(ups_iterator, upsender, [&]( auto& row ) {//URGENT This needs to be changed when we figure out the PK issue
+          row.totalups -= newups;
+      });
+    } else {
+      _ups.modify(ups_iterator, upsender, [&]( auto& row ) {//URGENT This needs to be changed when we figure out the PK issue
+          row.totalups += newups;
+      });
+    }
+
+    
+  }//END if(results _ups) 
 }
 
-void ups::logup(uint32_t upscount, name upsender, uint64_t content_id) {
+// --- Upsert _uppers and _totals --- //
+void upsert_total(uint32_t &upscount, name &upsender, uint32_t &content_id, bool negative) {
+  require_auth( upsender );
+
+  // --- Update / Insert _totals record of cumulative song Ups --- //
+  _totals(get_self(), content_id);
+  auto total_iterator = _totals.find(content_id);
+  uint32_t time_of_up = eosio::time_point_sec::sec_since_epoch();
+  if( total_iterator == _totals.end() )
+  { // -- Make New Record
+    _totals.emplace(upsender, [&]( auto& row ) {
+      row.key = content_id;
+      row.totalups = newups;
+      row.updated = time_of_up;
+    });
+  } 
+  else 
+  { // -- Update Record 
+    if(!negative){
+      _totals.modify(total_iterator, upsender, [&]( auto& row ) {
+        row.key = content_id;
+      row.totalups += newups;
+        row.updated = time_of_up;
+      });
+    } else { // Subtract the value from totals
+      _totals.modify(total_iterator, upsender, [&]( auto& row ) {
+        row.key = content_id;
+      row.totalups -= newups;
+        row.updated = time_of_up;
+      });
+    }
     
-}
+    
+  }//END if(results _totals)
+
+  // --- Update / Insert _uppers record --- //
+  _uppers(get_self(), content_id);
+  auto listener_iterator = _uppers.find(content_id);
+  if( listener_iterator == _uppers.end() )
+  {
+    _uppers.emplace(upsender, [&]( auto& row ) {
+      row.upsender = upsender;
+      row.firstup = time_of_up;
+      row.lastup = time_of_up;
+      row.totalups = newups;
+    });
+  } 
+  else 
+  {
+    _uppers.modify(listener_iterator, upsender, [&]( auto& row ) {
+      row.lastup = eosio::time_point_sec::sec_since_epoch();
+      row.totalups += newups;
+    });
+  }//END if(results _uppers)
+}//END upsert_total()
+
 
 void ups::removeiou(name sender, name receiver) {
     
 }
 
-void ups::updateupper(uint32_t upscount, name upsender) {
+void ups::upsertupper(uint32_t upscount, name upsender) {
     
 }
 
@@ -23,9 +130,5 @@ void ups::removecont(uint64_t content_id) {
 }
 
 void ups::deepremvcont(uint64_t content_id) {
-    
-}
-
-void ups::up_catch(const name from, const name to, const asset quantity, const std::string memo) {
     
 }
